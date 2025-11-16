e4090d706d62b9d77f0d1b545a146d4d2474011b
Devylp <jackvinston75@gmail.com>
2025-11-16 21:08:27 +0200

    Radix_sort

--- Сортировки/Lab_8.py
+++ Сортировки/Lab_8.py
@@ -1,7 +1,53 @@
 # №8 Поразрядная
 
+'''
+def bucketsort(array):
+    out = []
 
-def radix_sort(array):
-    pass
+    # Создаем список "вёдер" для распределения по числам от 0 до 9
+    buckets = [[] for _ in range(10)]
+
+    # Распределяем числа по "вёдрам"
+    for item in array:
+        buckets[item].append(item)
+
+    # Опустошаем "вёдра"
+    for bucket in buckets:
+        for item in bucket:
+            out.append(item)
+    
+    return out
+'''
+
+def bucket_sort_by_digit(array, exponent):
+    out = []
+    buckets = [[] for _ in range(10)]
+
+    for item in array:
+        digit = (item // exponent) % 10
+        buckets[digit].append(item)
+
+    for bucket in buckets:
+        for item in bucket:
+            out.append(item)
+    
+    return out
 
 
+def radix_sort(arr):
+    if not arr:
+        return arr
+        
+    max_element = max(arr)
+    exponent = 1
+    
+    while max_element // exponent > 0:
+        
+        arr = bucket_sort_by_digit(arr, exponent)
+        
+        exponent *= 10
+        
+    return arr
+
+
+print(radix_sort([12, 4, 890, 23, 1, 7, 2]))
